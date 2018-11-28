4cd9987a82e0bb9ebcd40556ec0fad7cf2fc01a6
wzshao <shawn.wzshao@gmail.com>
2018-11-28 15:07:58 -0500

    let us see..

--- plane_extraction_tracking/src/plane.cpp
+++ plane_extraction_tracking/src/plane.cpp
@@ -11,14 +11,12 @@
 
 #include <iostream>
 
-//void project_lidar_point();
- 
 cv::Mat colorSeg(const cv::Mat& img)
 {
 	// convert to HSV
 	cv::Mat img_hsv;
 	cv::cvtColor( img, img_hsv, CV_BGR2HSV );
-
+    // show HSV image
     cv::imshow( "window", img_hsv );
     cv::waitKey(0);
 
@@ -36,11 +34,9 @@ int main(int argc, char **argv)
     std::string image_path("/home/wz/Desktop/822/822_test/images/right_0_1528404291835066602.jpg");
     cv::Mat image = cv::imread(image_path, CV_LOAD_IMAGE_COLOR);   // assume BGR now. Make sure RGB or BGR
 
-    std::cout << "test size: " << image.size << std::endl;
+    std::cout << "size: " << image.size << std::endl;
 
 	cv::Mat imgSegged = colorSeg(image);
-    // do the projection and show results
-    //project_lidar_point();
 
     return 0;
 }
